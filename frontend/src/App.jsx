2a7138c525dcd7650e66ea30fd40260f26d3943d
William Warwick <warwickw8@gmail.com>
2024-04-24 15:17:37 +0200

    basic ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css"
+import SkipNextIcon from '@mui/icons-material/SkipNext'; 
+import PauseCircleIcon from '@mui/icons-material/PauseCircle';
 
 export default function App(){
-  return
+  return SoundBox();
 }
 
 function SoundBox(){
@@ -9,7 +11,7 @@ function SoundBox(){
     <div className = "sound-box">
     <Image />
     <SongInformation />
-    <CurrentTimeIndicator />
+    {/* <CurrentTimeIndicator /> */}
     <MediaControls />
 
   </div>
@@ -17,24 +19,36 @@ function SoundBox(){
 }
 
 function Image(){
-  return
-  <div>
-    
-  </div>
+  return(
+    <img className = "music-image" src="https://www.nme.com/wp-content/uploads/2016/09/62_radiohead-1.jpg" alt="Album Cover" />
+  );
 
 }
 
 function SongInformation(){
-  return
+  return <>
+    <div className = "song-info">
+      <label htmlFor="">In Rainbows</label>
+      <label htmlFor="">Radiohead</label>
+    </div>
+  </>
 
 }
 
 function CurrentTimeIndicator(){
   return
 
+  //how to make a currentimeindicator in javascript
 }
 
 function MediaControls(){
-  return
+  return(
+    <div>
+      <h1>
+       <PauseCircleIcon />
+      </h1>
+    </div>
+  );
+
 
 }
