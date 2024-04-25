b47799c12d06f4b872b0ea55ca41567a3009d70b
William Warwick <warwickw8@gmail.com>
2024-04-25 21:17:29 +0200

    static skip buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,9 @@
 import PauseCircleFilled from '@material-ui/icons/PauseCircleFilled'
 import Repeat from '@material-ui/icons/Repeat'
 import Shuffle from '@material-ui/icons/Shuffle'
+import SkipNext from '@material-ui/icons/SkipNext'
+import SkipPrevious from '@material-ui/icons/SkipPrevious'
 import "./App.css"
-
 
 
 export default function App(){
@@ -17,16 +18,35 @@ function SoundBox(){
     <SongInformation />
     <CurrentTimeIndicator />
     <MediaControls />
-
   </div>
   )
 }
 
 function Image(){
   return(
-    <img className = "music-image" src="https://www.nme.com/wp-content/uploads/2016/09/62_radiohead-1.jpg" alt="Album Cover" />
+    <div className = "skip-media">
+      <SkipPreviousSong />
+      <img className = "music-image" src="https://www.nme.com/wp-content/uploads/2016/09/62_radiohead-1.jpg" alt="Album Cover" />
+      <SkipNextSong />
+    </div>
   );
 
+}
+
+function SkipNextSong(){
+  return(
+    <div className = "skip-next">
+      <SkipNext style = {{fontSize: '5rem'}}/>
+    </div>
+  );
+}
+
+function SkipPreviousSong(){
+  return(
+    <div className = "skip-previous">
+      <SkipPrevious style = {{fontSize: '5rem'}}/>
+    </div>
+  );
 }
 
 function SongInformation(){
@@ -54,9 +74,11 @@ function MediaControls(){
       <div className = "shuffle-control">
         <Shuffle style = {{fontSize: '3.5rem'}}/>
       </div>
+
       <div className = "pause-control">
         <PauseCircleFilled style = {{fontSize: '5rem'}}/>
       </div>
+      
       <div className = "replay-control">
         <Repeat style = {{fontSize: '3.5rem'}}/>
       </div>
