7910ea1af1f84d21130c30674005255d4bd3e5ac
William Warwick <warwickw8@gmail.com>
2024-06-10 22:38:52 +0200

    working audio!!!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import PauseCircleFilled from '@material-ui/icons/PauseCircleFilled'
-import Repeat from '@material-ui/icons/Repeat'
-import Shuffle from '@material-ui/icons/Shuffle'
-import SkipNext from '@material-ui/icons/SkipNext'
-import SkipPrevious from '@material-ui/icons/SkipPrevious'
-import PlayCircleFilled from '@material-ui/icons/PlayCircleFilled'
-import "./App.css"
-import React, {useEffect, useState, useRef} from 'react'
+import PauseCircleFilled from '@material-ui/icons/PauseCircleFilled';
+import Repeat from '@material-ui/icons/Repeat';
+import Shuffle from '@material-ui/icons/Shuffle';
+import SkipNext from '@material-ui/icons/SkipNext';
+import SkipPrevious from '@material-ui/icons/SkipPrevious';
+import PlayCircleFilled from '@material-ui/icons/PlayCircleFilled';
+import "./App.css";
+import React, { useEffect, useState, useRef } from 'react';
 import { audios } from './audioData';
 
-export default function App(){
-  return SoundBox();
+export default function App() {
+  return <SoundBox />;
 }
 
-function SoundBox(){
-
+function SoundBox() {
   const [currentAudioIndex, setCurrentAudioIndex] = useState(0);
 
-  const handleNextSong = () =>{
+  const handleNextSong = () => {
     setCurrentAudioIndex((prevIndex) => (prevIndex + 1) % audios.length);
   };
 
@@ -26,76 +25,76 @@ function SoundBox(){
 
   const selectedAudio = audios[currentAudioIndex];
 
-  return(
-    <div className = "sound-box">
-    <Image image={selectedAudio.image} onNextClick={handleNextSong} onPreviousClick={handlePreviousSong}/>
-    <SongInformation name={selectedAudio.name} creator={selectedAudio.creator}/>
-    <CurrentTimeIndicator music = {selectedAudio.music}/>
-    <MediaControls />
-    </div>
-  )
-}
-
-function Image({ image, onNextClick, onPreviousClick }){
-  return(
-    <div className = "skip-media">
-      <SkipPreviousSong onPreviousClick={onPreviousClick}/>
-      <img className = "music-image" src={image} alt="Album Cover" />
-      <SkipNextSong onNextClick={onNextClick}/>
+  return (
+    <div className="sound-box">
+      <Image image={selectedAudio.image} onNextClick={handleNextSong} onPreviousClick={handlePreviousSong} />
+      <SongInformation name={selectedAudio.name} creator={selectedAudio.creator} />
+      <CurrentTimeIndicator />
+      <MediaControls music={selectedAudio.music} />
     </div>
   );
 }
 
-function SkipNextSong({onNextClick}){
-  return(
-    <div className = "skip-next" onClick={onNextClick}>
-      <SkipNext style = {{fontSize: '5rem'}}/>
+function Image({ image, onNextClick, onPreviousClick }) {
+  return (
+    <div className="skip-media">
+      <SkipPreviousSong onPreviousClick={onPreviousClick} />
+      <img className="music-image" src={image} alt="Album Cover" />
+      <SkipNextSong onNextClick={onNextClick} />
     </div>
   );
 }
 
-function SkipPreviousSong({onPreviousClick}){
-  return(
-    <div className = "skip-previous" onClick = {onPreviousClick}>
-      <SkipPrevious style = {{fontSize: '5rem'}}/>
+function SkipNextSong({ onNextClick }) {
+  return (
+    <div className="skip-next" onClick={onNextClick}>
+      <SkipNext style={{ fontSize: '5rem' }} />
     </div>
   );
 }
 
-function SongInformation({name, creator}){
-  return(
-    <div className = "song-info">
+function SkipPreviousSong({ onPreviousClick }) {
+  return (
+    <div className="skip-previous" onClick={onPreviousClick}>
+      <SkipPrevious style={{ fontSize: '5rem' }} />
+    </div>
+  );
+}
+
+function SongInformation({ name, creator }) {
+  return (
+    <div className="song-info">
       <h4>{name}</h4>
       <h4>{creator}</h4>
     </div>
   );
 }
 
-function CurrentTimeIndicator(){
-  return(
+function CurrentTimeIndicator() {
+  return (
     <hr />
   );
-
-  //how to make a currentimeindicator in javascript
 }
 
-function MediaControls({music}) {
+function MediaControls({ music }) {
   const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
 
   const togglePlay = () => {
-    setIsPlaying(!isPlaying);
+    setIsPlaying((prevIsPlaying) => !prevIsPlaying);
   };
 
-  const audioRef = useRef(null);
   useEffect(() => {
-    if(isPlaying){
-      audioRef.current.play();
+    if (audioRef.current) {
+      if (isPlaying) {
+        console.log('Playing audio:', music);
+        audioRef.current.play().catch(error => console.error('Error playing audio:', error));
+      } else {
+        console.log('Pausing audio');
+        audioRef.current.pause();
+      }
     }
-    else{
-      audioRef.current.pause();
-    }
-  }, [isPlaying])
-
+  }, [isPlaying, music]);
 
   return (
     <div className="media-control">
@@ -107,17 +106,11 @@ function MediaControls({music}) {
 
       {isPlaying ? (
         <div className="pause-play-control">
-          <PauseCircleFilled
-            onClick={togglePlay}
-            style={{ fontSize: '7rem' }}
-          />
+          <PauseCircleFilled onClick={togglePlay} style={{ fontSize: '7rem' }} />
         </div>
       ) : (
         <div className="pause-play-control">
-          <PlayCircleFilled
-            onClick={togglePlay}
-            style={{ fontSize: '7rem' }}
-          />
+          <PlayCircleFilled onClick={togglePlay} style={{ fontSize: '7rem' }} />
         </div>
       )}
 
--- frontend/src/audioData.js
+++ frontend/src/audioData.js
@@ -1,21 +1,24 @@
+import InRainbows from './mp3-files/InRainbows.mp3';
+import Blond from './mp3-files/Blond.mp3';
+import Rumours from './mp3-files/Rumours.mp3';
+
 export const audios = [
     {
         name: 'In Rainbows',
-        music: './mp3-files/InRainbows.mp3',
+        music: InRainbows,
         creator: 'Radiohead',
         image: 'https://www.nme.com/wp-content/uploads/2016/09/62_radiohead-1.jpg'
     },
     {
         name: 'Blond',
-        music: './mp3-files/Blond.mp3',
+        music: Blond,
         creator: 'Frank Ocean',
         image: 'https://upload.wikimedia.org/wikipedia/en/a/a0/Blonde_-_Frank_Ocean.jpeg'
     },
     {
         name: 'Rumours',
-        music: './mp3-files/Rumours.mp3',
+        music: Rumours,
         creator: 'Fleetwood Mac',
         image: 'https://upload.wikimedia.org/wikipedia/en/f/fb/FMacRumours.PNG'
     }
-
-]
+];
