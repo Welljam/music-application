743019129db06278ae0ef81fdb754c9cca012393
William Warwick <warwickw8@gmail.com>
2024-06-12 16:43:06 +0200

    const formatTime

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -100,6 +100,17 @@ function CurrentTimeIndicator({progressBarRef, timeProgress, duration}) {
   // );
 }
 
+const formatTime = (time) => {
+  if(time && !isNaN(time)){
+    const minutes = Math.floor(time/60);
+    const formatMinutes = minutes < 10 ? '0${minutes}' : '${minutes}';
+    const seconds = Math.floor(time%60);
+    const formatSeconds = seconds < 10 ? '0${seconds}' : '${seconds}';
+    return '${formatMinutes}:${formatSeconds}';
+  } 
+  return '00:00';
+};
+
 function MediaControls({ music, audioRef, setDuration, progressBarRef }) {
   const [isPlaying, setIsPlaying] = useState(false);
   // const audioRef = useRef(null);
@@ -114,9 +125,7 @@ function MediaControls({ music, audioRef, setDuration, progressBarRef }) {
     const seconds = audioRef.current.duration;
     setDuration(seconds);
     progressBarRef.current.max = seconds;
-
   }
-
 
   useEffect(() => {
     if (audioRef.current) {
