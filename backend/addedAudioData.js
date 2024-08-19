421e6c6179a880aee0c6b6d9947f070060486b27
William Warwick <warwickw8@gmail.com>
2024-08-19 17:53:54 +0200

    add new songs to a json file

---
+++ backend/addedAudioData.js
@@ -0,0 +1,8 @@
+[
+  {
+    "artist": "Lanadelrey",
+    "songID": "4",
+    "songName": "ultraviolence",
+    "imageUrl": "blob:http://localhost:5173/2c86b2ab-0b52-4f79-923e-a9806abe18d2"
+  }
+]
--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,46 @@ const port = 3001;
 const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
+const audioData = path.join(__dirname, 'addedAudioData.js');
 
 app.use(cors());
 app.use(express.json());
+
+function readSongsFromFile(){
+    try{
+        const data = fs.readFileSync(audioData, 'utf-8');
+        return JSON.parse(data);
+    } catch(err){
+        return [];
+    }
+}
+
+function writeSongsToFile(songs){
+    fs.writeFileSync(audioData, JSON.stringify(songs, null, 2), 'utf-8');
+}
 
 app.get('/', (req, res) => {
     res.send('Welcome to the backend!');
 });
 
 app.get('/songs', (req, res) => {
-    res.send('song information');
+    const songs = readSongsFromFile();
+    res.json(songs);
 });
 
 app.post('/add-song', (req, res) => {
     const newSong = req.body;
     console.log("Received new song data:", newSong);
+
+    //read existing songs
+    const songs = readSongsFromFile();
+
+    //add new song to json
+    songs.push(newSong);
+
+    //write the updated json to the file
+    writeSongsToFile(songs);
+
     res.status(200).json({ message: "Song data received" });
 });
 
