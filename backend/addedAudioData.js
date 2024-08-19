ee466c96bad6656671cd5e57f2cd7f96ffa58915
William Warwick <warwickw8@gmail.com>
2024-08-19 18:25:19 +0200

    added songs

--- backend/addedAudioData.js
+++ backend/addedAudioData.js
@@ -4,5 +4,17 @@
     "songID": "4",
     "songName": "ultraviolence",
     "imageUrl": "blob:http://localhost:5173/2c86b2ab-0b52-4f79-923e-a9806abe18d2"
+  },
+  {
+    "artist": "the sundays",
+    "songID": "4",
+    "songName": "life goes on",
+    "imageUrl": "blob:http://localhost:5173/69bb742f-396a-4781-8ecb-1024a9c2eff9"
+  },
+  {
+    "artist": "lana del rey",
+    "songID": "5",
+    "songName": "ultra violence",
+    "imageUrl": "blob:http://localhost:5173/f1501e2c-2cb2-4a0b-a1fe-b737789c319a"
   }
 ]
