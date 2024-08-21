63e93ca6c5ee6d0c72239229e28862e3201de191
William Warwick <warwickw8@gmail.com>
2024-08-21 11:47:14 +0200

    quick changes

--- backend/addedAudioData.js
+++ backend/addedAudioData.js
@@ -16,5 +16,11 @@
     "songID": "5",
     "songName": "ultra violence",
     "imageUrl": "blob:http://localhost:5173/f1501e2c-2cb2-4a0b-a1fe-b737789c319a"
+  },
+  {
+    "artist": "the sundays",
+    "songID": "4",
+    "songName": "life goes on",
+    "imageUrl": "blob:http://localhost:5173/c3ff5914-dc9f-40b4-a908-71e1dedf5710"
   }
 ]
